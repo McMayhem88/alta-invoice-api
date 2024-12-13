dfe7638649833719d01cc48ded5a4bd5c81bf359
McMayhem88 <cmcmackin88@hotmail.com>
2024-12-12 21:55:12 -0800

    Added more documentation

--- backend-project/src/invoices/dto/invoice-id.dto.ts
+++ backend-project/src/invoices/dto/invoice-id.dto.ts
@@ -5,6 +5,9 @@ import {Type} from 'class-transformer';
  * DTO for invoice input validation
  */
 export class InvoiceIdDto { //!! I really only use DTOs for more complex objects and POST endpoints, but I felt it was necessary
+  /**
+   * The unique `id` of the invoice
+   */
   @Type(() => Number)
   @IsInt({message: 'Invoice ID must be a number'})
   @Min(0)
--- backend-project/src/invoices/dto/pagination.dto.ts
+++ backend-project/src/invoices/dto/pagination.dto.ts
@@ -5,12 +5,18 @@ import {Transform} from "class-transformer";
  * DTO to validate pagination parameters passed to the invoice list function
  */
 export class PaginationDto {
+  /**
+   * Represents the current page of the pagination
+   */
   @IsOptional()
   @Transform(({value}) => parseInt(value, 10))
   @IsInt()
   @Min(0)
   page?: number = 0; // `0` will be used as an indicator that pagination is disabled.
   
+  /**
+   * The maximum number of records that can be displayed on a single page when paginated
+   */
   @IsOptional()
   @Transform(({value}) => parseInt(value, 10))
   @IsInt()
